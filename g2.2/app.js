febe08298b64229b602dcb956c6d81ee22a04ac0
LanceEddleman <ozzland_dracor@hotmail.com>
2016-02-17 14:48:12 -0700

    v2 of gunters team app

---
+++ g2.2/app.js
@@ -0,0 +1,59 @@
+// Variables
+    console.debug();
+    var sectionTitle = '<div class="fRow"><div class="tasks">Tasks</div><div class="notes">Notes</div></div>';
+    var fireText = $("#fireText");
+    var nextText = $("#nextText");
+    var fireData = $('#fireData')
+    var nextData = $('#nextData')
+
+ // Current Date
+   var today = new Date();
+   var viewToday = (
+       (today.getMonth() + 1) + "/" +
+       today.getDate() + "/" +
+       today.getFullYear()
+   );
+   console.log('Today is: ' + viewToday);
+
+
+// Fire -------------------------
+   function fireStart(){
+     console.log('pull Gunters data');
+     var fireText = $("#fireText");
+   };
+
+    domo.get('/data/v1/gZone?filter=status = "Fire"').then(function(gZone){
+           console.log("gZone", gZone);
+
+    fireText.append(sectionTitle); // add title section
+
+    gZone.forEach(function(item) {
+//    var rCount = 1;
+        // console.log(item);
+//        if(rCount == 1) {
+//            fireData.append('<div class="fRow lightG"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');
+//		}
+//		else {
+			// fireData.append('<div class="fRow"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');	
+			fireData.append('<div class="fRow"><div>' + item.task + '</div><div>'+ item.notes +'</div></div>');	
+//		}
+       });
+    });
+
+// next -------------------------
+   function nextStart(){
+     console.log('pull Gunters data');
+     var nextText = $("#nextText");
+   };
+
+    domo.get('/data/v1/gZone?filter=status = "Next"').then(function(gZone){
+           console.log("gZone", gZone);
+
+    nextText.append(sectionTitle); // add title section
+
+    gZone.forEach(function(item) {
+        console.log(item);
+        nextData.append('<div class="fRow"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');
+       });
+    });
+
