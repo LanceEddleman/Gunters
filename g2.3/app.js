35a65e8e25f65607eb478935ab6998a3da537220
LanceEddleman <ozzland_dracor@hotmail.com>
2016-02-25 14:31:32 -0700

    cleaning up to new format and layout

--- g2.3/app.js
+++ g2.3/app.js
@@ -5,8 +5,8 @@
     var sectionTitle = '<div class="fRow"><div class="tasks">Tasks</div><div class="notes">Notes</div></div>';
     var fireText = $("#fireText");
     var nextText = $("#nextText");
-    var fireData = $('#fireData')
-    var nextData = $('#nextData')
+    var fireData = $('#fireData');
+    var nextData = $('#nextData');
 
  // Current Date
    var today = new Date();
@@ -19,43 +19,51 @@
 
 
 // Fire -------------------------
-   function fireStart(){
-     console.log('pull Gunters data');
-     var fireText = $("#fireText");
-   };
+	function fireStart(){
+		var rCount = 1;
+		console.log('pull Fire data');
+		var fireText = $("#fireText");
 
-    domo.get('/data/v1/gZone?filter=status = "Fire"').then(function(gZone){
-           console.log("gZone", gZone);
-
-    fireText.append(sectionTitle); // add title section
-
-    gZone.forEach(function(item) {
-//    var rCount = 1;
-        // console.log(item);
-//        if(rCount == 1) {
-//            fireData.append('<div class="fRow lightG"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');
-//		}
-//		else {
-			// fireData.append('<div class="fRow"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');	
-			fireData.append('<div class="fRow"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');	
-//		}
-       });
-    });
+		domo.get('/data/v1/gZone?filter=status = "Fire"').then(function(gZone){
+//			console.log("gZone", gZone);
+			fireText.append(sectionTitle); // add title section
+			gZone.forEach(function(item) {
+				if(rCount % 2 === 0) {
+					fireData.append('<div class="fRowE"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>'); 
+					rCount = rCount+1;
+					console.log('evenRow:' + rCount);
+				}
+				else {
+					fireData.append('<div class="fRow"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');	
+					rCount = rCount+1;
+					console.log('oddRow:' + rCount);
+				}
+			});
+		});
+//		nextStart();
+	};
 
 // next -------------------------
-   function nextStart(){
-     console.log('pull Gunters data');
-     var nextText = $("#nextText");
-   };
+	function nextStart(){
+		var rCount = 1;
+		console.log('pull Next data');
+		var nextText = $("#nextText");
 
-    domo.get('/data/v1/gZone?filter=status = "Next"').then(function(gZone){
-           console.log("gZone", gZone);
-
-    nextText.append(sectionTitle); // add title section
-
-    gZone.forEach(function(item) {
-        console.log(item);
-        nextData.append('<div class="fRow"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');
-       });
-    });
-
+		domo.get('/data/v1/gZone?filter=status = "Next"').then(function(gZone){
+//			console.log("gZone", gZone);
+			nextText.append(sectionTitle); // add title section
+			gZone.forEach(function(item) {
+				if(rCount % 2 === 0) {
+					console.log(item);
+					nextData.append('<div class="fRowE"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');
+					rCount = rCount+1;
+					console.log('evenRow:' + rCount);
+				}
+				else {
+					nextData.append('<div class="fRow"><div class="tasks">' + item.task + '</div><div class="notes">'+ item.notes +'</div></div>');
+					rCount = rCount+1;
+					console.log('oddRow:' + rCount);
+				}
+			});
+		});
+	};
